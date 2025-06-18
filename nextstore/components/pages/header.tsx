1e3bfe25005c7a5173f061cca4e658fa9386f04b
Miracle <godbeerus@mail.ru>
2025-06-18 16:33:27 +0300

    Add new styles

--- nextstore/components/pages/header.tsx
+++ nextstore/components/pages/header.tsx
@@ -1,16 +1,27 @@
 import '@/components/assets/css/header.css'
 import '@/components/assets/css/section.css'
 
+import Link from 'next/link'
+
+
 export default function Header() {
 	return (
-		<div className='header_container flex justify-around items-center'>
-			<article>
-				<a href='/'>Return to Home</a>
+		<div className='header_container flex justify-around '>
+			<article className='header-return p-1'>
+				<Link href='/'>Return to Home</Link>
 			</article>
 
-			<article>
+			<article className='header-search p-1'>
 				<input placeholder='Search...' />
 			</article>
+			<div className='header-interf flex flex-row gap-12'>
+				<Link href={`/`}>
+					<img src='https://img.icons8.com/parakeet-line/48/gender-neutral-user.png' />
+				</Link>
+				<Link href={`/`}>
+					<img src='/img/basket.png' alt='' />
+				</Link>
+			</div>
 		</div>
 	)
 }
